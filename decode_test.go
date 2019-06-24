a8c47487231269ec2d5359bbca93b547f445f680
Nick Dumas <dumasn@homes.com>
2019-06-24 15:45:13 -0400

    Moving to more table driven tests.

--- decode_test.go
+++ decode_test.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	"testing"
 )
 
 func TestItemDecode(t *testing.T) {
-	t.Run("simple item", func(t *testing.T) {
-		in := `{"verified": false, "w": 1, "h": 1, "ilvl": 45, "icon": "http://web.poecdn.com/image/Art/2DItems/Rings/Ring4.png?scale=1&w=1&h=1&v=6ef89e2afcf83dc3cc86b1522597ed2b", "league": "Standard", "id": "0ef6377fbeb361d2397d260047fbf6e1d4fb9f4c85cb56a3d9ed24bc5a311cc3", "name": "Kraken Grasp", "typeLine": "Gold Ring", "identified": true, "requirements": [{"name": "Level", "values": [["33", 0]], "displayMode": 0}], "implicitMods": ["6% increased Rarity of Items found"], "explicitMods": ["+11 to Strength", "+17 to maximum Energy Shield", "+33 to maximum Life", "46% increased Mana Regeneration Rate", "13% increased Rarity of Items found"], "frameType": 2, "category": {"accessories": ["ring"]}, "x": 6, "y": 3, "inventoryId": "Stash1"}`
+	types := []string{"ring", "gem", "sword"}
+	for _, itemType := range types {
+		t.Run(itemType, func(t *testing.T) {
+			f, err := os.Open("example/" + itemType + ".json")
+			defer f.Close()
 
-		var item Item
+			if err != nil {
+				t.Logf("could not open test data: %s", err)
+				t.Fail()
+			}
 
-		err := json.Unmarshal([]byte(in), &item)
-		if err != nil {
-			t.Logf("unmarshal error: %s", err)
-			t.Fail()
-		}
-	})
+			var item Item
+			d := json.NewDecoder(f)
+
+			err = d.Decode(&item)
+			if err != nil {
+				t.Logf("unmarshal error: %s", err)
+				t.Fail()
+			}
+		})
+	}
 }
 
 func TestRequirementsDecode(t *testing.T) {
