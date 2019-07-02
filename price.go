484d603908cb220b858c66da3f2917a9da85c4d1
Nick Dumas <nicholas.dumas@homes.com>
2019-07-02 09:10:20 -0400

    Implement and unit test price parsing.
    This one was a little tricky. Had to figure out how I wanted to
    store/express the currency conversion rates, so PriceDB is a thing.
    
    Should be reasonably extensible if there are any "price types" I've
    overlooked.

---
+++ price.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// https://api.poe.watch/get?league=Legion&category=currency
+
+var (
+	ErrInvalidCurrencyQuantity = errors.New("unrecognized currency quantity")
+	ErrUnrecognizedPriceType   = errors.New("unrecognized price type")
+	ErrUnrecognizedCurrency    = errors.New("unrecognized currency type")
+)
+
+func PriceCheck() ([]PricePoint, error) {
+	var prices []PricePoint
+
+	queryValues := url.Values{}
+	queryValues.Set("league", "Legion")
+	queryValues.Set("category", "currency")
+
+	PricesEndpoint := url.URL{
+		Host:     "api.poe.watch",
+		Scheme:   "https",
+		Path:     "get",
+		RawQuery: queryValues.Encode(),
+	}
+
+	c := http.Client{}
+
+	resp, err := c.Get(PricesEndpoint.String())
+	if err != nil {
+		return prices, err
+	}
+	defer resp.Body.Close()
+
+	d := json.NewDecoder(resp.Body)
+	err = d.Decode(&prices)
+	if err != nil {
+		return prices, err
+	}
+
+	return prices, nil
+}
+
+type PricePoint struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Category  string    `json:"category"`
+	Group     string    `json:"group"`
+	Frame     int       `json:"frame"`
+	StackSize int       `json:"stackSize,omitempty"`
+	Icon      string    `json:"icon"`
+	Mean      float64   `json:"mean"`
+	Median    float64   `json:"median"`
+	Mode      float64   `json:"mode"`
+	Min       float64   `json:"min"`
+	Max       float64   `json:"max"`
+	Exalted   float64   `json:"exalted"`
+	Total     int       `json:"total"`
+	Daily     int       `json:"daily"`
+	Current   int       `json:"current"`
+	Accepted  int       `json:"accepted"`
+	Change    float64   `json:"change"`
+	History   []float64 `json:"history"`
+	Type      string    `json:"type,omitempty"`
+}
+
+type PriceStatus int
+
+const (
+	Negotiable PriceStatus = iota
+	Exact
+	BetterOffer
+)
+
+type ItemPrice struct {
+	PriceStatus PriceStatus
+	Cost        float64
+}
+
+type PriceDB interface {
+	Convert(string, string) (float64, error)
+}
+
+func ParsePrice(s string, db PriceDB) (ItemPrice, error) {
+	var ip ItemPrice
+
+	fields := strings.Split(s, " ")
+	switch fields[0] {
+	case "-price":
+		ip.PriceStatus = Exact
+	case "~price":
+		ip.PriceStatus = Negotiable
+	case "~b/o":
+		ip.PriceStatus = BetterOffer
+	default:
+		return ip, ErrUnrecognizedPriceType
+	}
+
+	cost, err := db.Convert(fields[1], fields[2])
+	if err != nil {
+		return ip, err
+	}
+	ip.Cost = cost
+
+	return ip, nil
+}
---
+++ price_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+type DummyPriceDB struct {
+	priceMap map[string]float64
+}
+
+func (db DummyPriceDB) Convert(q string, t string) (float64, error) {
+
+	c, ok := db.priceMap[t]
+	if !ok {
+		return 0.0, ErrUnrecognizedCurrency
+	}
+
+	v, err := strconv.ParseFloat(q, 64)
+	if err != nil {
+		return 0.0, ErrInvalidCurrencyQuantity
+	}
+
+	return c * v, nil
+}
+
+func Test_ParsePrice(t *testing.T) {
+	var db DummyPriceDB
+	db.priceMap = make(map[string]float64)
+	db.priceMap["chaos"] = 1.0
+	db.priceMap["exa"] = 100.0
+	db.priceMap["alt"] = 0.01
+
+	t.Run("fixed", func(t *testing.T) {
+		cases := []struct {
+			in       string
+			expected ItemPrice
+		}{
+			{"-price 1 chaos", ItemPrice{Exact, 1.0}},
+			{"-price 1.5 chaos", ItemPrice{Exact, 1.5}},
+			{"-price 1.5 exa", ItemPrice{Exact, 150.0}},
+			{"-price 25 alt", ItemPrice{Exact, 0.25}},
+		}
+
+		for _, tt := range cases {
+			actual, err := ParsePrice(tt.in, db)
+			if err != nil {
+				t.Logf("unable to parse price: %s", err)
+				t.Fail()
+			}
+			if tt.expected != actual {
+				t.Logf("Expected `%#v`, received `%#v`", tt.expected, actual)
+				t.Fail()
+			}
+		}
+	})
+
+	t.Run("negotiable", func(t *testing.T) {
+		cases := []struct {
+			in       string
+			expected ItemPrice
+		}{
+			{"~price 1 chaos", ItemPrice{Negotiable, 1.0}},
+			{"~price 1.5 chaos", ItemPrice{Negotiable, 1.5}},
+			{"~price 1.5 exa", ItemPrice{Negotiable, 150.0}},
+			{"~price 25 alt", ItemPrice{Negotiable, 0.25}},
+		}
+
+		for _, tt := range cases {
+			actual, err := ParsePrice(tt.in, db)
+			if err != nil {
+				t.Logf("unable to parse price: %s", err)
+				t.Fail()
+			}
+			if tt.expected != actual {
+				t.Logf("Expected `%#v`, received `%#v`", tt.expected, actual)
+				t.Fail()
+			}
+		}
+	})
+
+	t.Run("best offer", func(t *testing.T) {
+		cases := []struct {
+			in       string
+			expected ItemPrice
+		}{
+			{"~b/o 1 chaos", ItemPrice{BetterOffer, 1.0}},
+			{"~b/o 1.5 chaos", ItemPrice{BetterOffer, 1.5}},
+			{"~b/o 1.5 exa", ItemPrice{BetterOffer, 150.0}},
+			{"~b/o 25 alt", ItemPrice{BetterOffer, 0.25}},
+		}
+
+		for _, tt := range cases {
+			actual, err := ParsePrice(tt.in, db)
+			if err != nil {
+				t.Logf("unable to parse price: %s", err)
+				t.Fail()
+			}
+			if tt.expected != actual {
+				t.Logf("Expected `%#v`, received `%#v`", tt.expected, actual)
+				t.Fail()
+			}
+		}
+	})
+}
