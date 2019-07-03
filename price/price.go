fc1d1d1455970616a88d08633f874dde0e41aec0
Nick Dumas <dumasn@homes.com>
2019-07-03 08:59:21 -0400

    Separating concerns.
    ParsePrice should exclusively extra price info from the note string.

--- price/price.go
+++ price/price.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	ErrInvalidCurrencyQuantity = errors.New("unrecognized currency quantity")
+	ErrBadParse                = errors.New("error parsing price")
 	ErrUnrecognizedPriceType   = errors.New("unrecognized price type")
 	ErrUnrecognizedCurrency    = errors.New("unrecognized currency type")
 )
@@ -22,30 +23,44 @@ const (
 )
 
 type ItemPrice struct {
-	PriceStatus PriceStatus
-	Cost        float64
+	PriceStatus  PriceStatus
+	BaseCurrency string
+	Cost         float64
 }
 
-func ParsePrice(s string, db PriceDB) (ItemPrice, error) {
+func ParsePrice(s string) (ItemPrice, error) {
 	var ip ItemPrice
 
 	fields := strings.Split(s, " ")
+
+	if s == "~price" {
+		ip.PriceStatus = Negotiable
+		return ip, nil
+	}
+
+	if len(fields) < 3 {
+		return ip, ErrBadParse
+	}
+
 	switch fields[0] {
 	case "-price":
 		ip.PriceStatus = Exact
 	case "~price":
 		ip.PriceStatus = Negotiable
-	case "~b/o":
+	case "~b/o", "b/o":
 		ip.PriceStatus = BetterOffer
 	default:
 		return ip, ErrUnrecognizedPriceType
 	}
 
-	cost, err := db.Convert(fields[1], fields[2])
-	if err != nil {
-		return ip, err
-	}
-	ip.Cost = cost
+	/*
+		cost, err := db.Convert(fields[1], fields[2])
+		if err != nil {
+			return ip, err
+		}
+		ip.Cost = cost
+	*/
+	ip.BaseCurrency = fields[2]
 
 	return ip, nil
 }
